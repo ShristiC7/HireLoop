bf4c3aa002df7a8a146e15216bcc00eed107131c
Shubham Verma <sv0795167@gmail.com>
2026-04-10 20:37:19 +0530

    Added frontend from campus-connect-hub

---
+++ playwright.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './src/test',
+  fullyParallel: true,
+  forbidOnly: !!process.env['CI'],
+  retries: process.env['CI'] ? 2 : 0,
+  workers: process.env['CI'] ? 1 : undefined,
+});
